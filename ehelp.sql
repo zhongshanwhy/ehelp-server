5eb58876b7602710b92e6a33253d88d491e27f0f
wypstudy <tangzhongwhy@gmail.com>
2015-08-07 17:52:00 +0900

    adopt to lunux

--- ehelp.sql
+++ ehelp.sql
@@ -1,4 +1,4 @@
-/*
+
 DROP TABLE IF EXISTS `alluser`;
 CREATE TABLE `alluser` (
   `user_id` varchar(45) NOT NULL,
@@ -9,18 +9,18 @@ CREATE TABLE `alluser` (
   `gender` varchar(10) DEFAULT NULL,
   `health_state` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-)
+);
 
-
+DROP TABLE IF EXISTS `question`;
 CREATE TABLE `question` (
   `question_id` varchar(45) NOT NULL,
   `question_content` text(500) DEFAULT NULL,
   `question_type` int(11) NOT NULL,
   `question_time` datetime DEFAULT NULL,
   PRIMARY KEY (`question_id`)
-)
+);
 
-
+DROP TABLE IF EXISTS `answer`;
 CREATE TABLE answer (
   `answer_id` varchar(45) NOT NULL,
   `question_id` varchar(45) NOT NULL,
@@ -29,18 +29,18 @@ CREATE TABLE answer (
   `answer_time` datetime DEFAULT NULL,
   PRIMARY KEY (`question_id`),
   CONSTRAINT `question_answer_tuple_fk` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`) ON DELETE CASCADE ON UPDATE CASCADE
-)
-*/
+);
 
+DROP TABLE IF EXISTS `friendlist`;
 CREATE TABLE `friendlist` (
   `host_name` varchar(45) NOT NULL,
   `guest_name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`host_name`),
   CONSTRAINT friend_host_fk FOREIGN KEY (`host_name`) REFERENCES alluser (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT friend_guest_fk FOREIGN KEY (`guest_name`) REFERENCES alluser (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-)
-/**/
+);
 
+DROP TABLE IF EXISTS `bank`;
 CREATE TABLE `bank` (
   `bank_id` int(11) NOT NULL AUTO_INCREMENT,
   `bank_name` varchar(45) NOT NULL,
@@ -48,8 +48,9 @@ CREATE TABLE `bank` (
   `coin` int(11) DEFAULT 20,
   PRIMARY KEY (`bank_id`)
   CONSTRAINT bank_name_fk FOREIGN KEY (`bank_name`) REFERENCES alluser (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-)
+);
 
+DROP TABLE IF EXISTS sign;
 CREATE TABLE `sign` (
   `sign_id` int(11) NOT NULL AUTO_INCREMENT,
   `sign_name` varchar(45) NOT NULL,
@@ -57,4 +58,4 @@ CREATE TABLE `sign` (
   `sign_time` date DEFAULT NULL,
   PRIMARY KEY (`sign_id`),
   CONSTRAINT sign_name_fk FOREIGN KEY (`sign_name`) REFERENCES alluser (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-)
+);
